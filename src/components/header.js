19540723b1a1e0c2528a0b3e05ecffe58195ed74
Hunter315 <hunter.mckay.smith@gmail.com>
2019-02-27 09:10:18 -0700

    SVG background header

--- src/components/header.js
+++ src/components/header.js
@@ -6,64 +6,63 @@ const Header = ({ siteTitle }) => (
   <header
     style={{
       marginBottom: `1.45rem`,
-      height: '80vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }} className = "bg-pattern"
+      height: "80vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexFlow: "column",
+    }}
+    className="bg-pattern"
   >
-  
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexFlow:'column wrap'
-      }}
-    ><span>
-      <h1 style={{ margin: 0 }}>
+    <nav className="my-nav" >
+      <h2 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `black`,
+            color: `white`,
             textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <p className = "subtitle">A fullstack web developer.</p>
-      </span>
-
-      <h2 style={{ margin: 0, position: 'absolute', left: 0 }} className = "rotate">
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          
           }}
         >
           About
         </Link>
       </h2>
-      <h2 style={{ margin: 0, position: 'absolute', right: 0 }} className = "rotateNeg">
+      <h2 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `black`,
+            color: `white`,
             textDecoration: `none`,
-          
           }}
         >
           Projects
         </Link>
       </h2>
-
-      
+    </nav>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexFlow: "column wrap",
+      }}
+    >
+      <span>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <p className="subtitle">A fullstack web developer.</p>
+      </span>
     </div>
   </header>
 )
